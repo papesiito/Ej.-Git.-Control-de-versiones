b08069f1881c362e464125843383524cc06c18ec
Pape <papealioune.gayediouf@gmail.com>
2024-09-30 18:24:45 +0200

    initial

---
+++ app.js
@@ -0,0 +1,11 @@
+const http = require("http");
+const port = 3000;
+
+http.createServer((req, res) => {
+
+    res.write (`Hola Mundo desde el servidor!´);
+     res.end();
+})
+.listen(port, ()=> {
+    console.log(`Servidor escuchando en: http://localhost:${port}`)
+})
